b8835a780d61def6356c588a6ec25a7ea14e7fa2
SamsonKnightz <113211927+SamsonKnightz@users.noreply.github.com>
2023-02-20 18:35:57 -0500

    pulled portfolio container through with header.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
+import PortfolioContainer from "./components/PortfolioContainer";
 import Header from "./components/Header";
 import './App.css';
+
+
 
 function App() {
   return (
     <div>
       <Header/>
+      <PortfolioContainer />
+      <footer/>
     </div>
 
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -10,9 +10,10 @@ function Header() {
               className="d-block w-100"
               src="https://cdn.pixabay.com/photo/2016/05/05/02/37/sunset-1373171_960_720.jpg"
               alt="First slide"
+              height="300"
             />
             <Carousel.Caption>
-              <h3>First slide label</h3>
+              <h3 className="text-white fw-bolder">First slide label</h3>
               <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
             </Carousel.Caption>
           </Carousel.Item>
@@ -21,10 +22,11 @@ function Header() {
               className="d-block w-100"
               src="https://cdn.pixabay.com/photo/2017/02/08/17/24/fantasy-2049567_960_720.jpg"
               alt="Second slide"
+              height="300"
             />
     
             <Carousel.Caption>
-              <h3>Second slide label</h3>
+              <h3 className="text-white fw-bolder">Second slide label</h3>
               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
             </Carousel.Caption>
           </Carousel.Item>
@@ -33,10 +35,11 @@ function Header() {
               className="d-block w-100"
               src="https://cdn.pixabay.com/photo/2018/01/14/23/12/nature-3082832_960_720.jpg"
               alt="Third slide"
+              height="300"
             />
     
             <Carousel.Caption>
-              <h3>Third slide label</h3>
+              <h3 className="text-white fw-bolder">Third slide label</h3>
               <p>
                 Praesent commodo cursus magna, vel scelerisque nisl consectetur.
               </p>
