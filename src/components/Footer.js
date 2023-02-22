75d18d2c0641abef9ca49f010fdc4aa3258b65d4
SamsonKnightz <113211927+SamsonKnightz@users.noreply.github.com>
2023-02-22 16:28:48 -0500

    changed footer, creating readme, created footer.css

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,28 +1,23 @@
 import React from "react"
+import "../../src/styles/Footer.css"
 
 const styles = {
-    sectionStyles: {
-      background: 'black',
-      color: "white",
-  
+    footerStyles: {
+        background: 'black',
+        color: "white",
+
     },
-  };
+};
 
 function Footer() {
     return (
-        <section style={styles.sectionStyles} className="section">
-        <div class="container">
-            <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top sticky-bottom">
-                <p class="col-md-4 mb-0 text-muted">© 2023 Samson Vang</p>
-
-                <ul class="nav col-md-4 justify-content-end">
-                    <li class="nav-item"><a href="https://github.com/SamsonKnightz" target="_" class="nav-link px-2 text-muted">Github</a></li>
-                    <li class="nav-item"><a href="https://stackoverflow.com/users/21252899/samson-vang" target="_" class="nav-link px-2 text-muted">Stack Overflow</a></li>
-                    <li class="nav-item"><a href="https://www.linkedin.com/in/samson-vang/" target="_" class="nav-link px-2 text-muted">LinkedIn</a></li>
-                </ul>
-            </footer>
-        </div>
-        </section>
+        <footer style={styles.footerStyles} class="bg-black text-center text-white">
+            <div class="text-center p-3" >
+                © 2023 Copyright
+                <br></br>
+                <a class="text-white" href="https://mdbootstrap.com/">Samson J Vang</a>
+            </div>
+        </footer>
     );
 }
 
