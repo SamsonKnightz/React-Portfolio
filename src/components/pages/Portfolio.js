19772869e080813f55a142f4aa1116bc5a6a9ab9
SamsonKnightz <113211927+SamsonKnightz@users.noreply.github.com>
2023-02-22 19:56:16 -0500

    finally got cards looking the way I want it

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -3,8 +3,7 @@ import '../../styles/Portfolio.css';
 
 const styles = {
   portfolioStyles: {
-    background: "black",
-    color: "white",
+
 
   },
 };
@@ -13,38 +12,38 @@ const styles = {
 
 export default function Portfolio() {
   return (
-    <div class="bg-black row row-cols-1 row-cols-md-6 g-6 p-3">
+    <div class="bg-black row row-cols-1 row-cols-md-6 g-6 p-3" >
       <div class="col">
-        <div class="bg-black card h-100 ">
+        <div class="bg-black card h-100 " className="portfolio" style={styles.portfolioStyles}>
           <img src="https://mdbcdn.b-cdn.net/img/new/standard/city/041.webp" class="card-img-top" alt="Hollywood Sign on The Hill" />
           <div class="card-body">
-            <h5 class="card-title text-white text-center">Card title</h5>
+            <h5 class="card-title text-white text-center p-4">Card title</h5>
             <p class="card-text text-white">
               Project 1
             </p>
             <p>
-            <a type="button" class="btn btn-light btn-rounded" href="https://github.com/" style={styles.portfolioStyles}>Light</a>
-            <a type="button" class="btn btn-light btn-rounded" href="https://github.com/" style={styles.portfolioStyles}>Light</a>
+            <a type="button" class="btn btn-light btn-rounded" href="https://github.com/" >Light</a>
+            <a type="button" class="btn btn-light btn-rounded" href="https://github.com/" >Light</a>
             </p>
           </div>
         </div>
       </div>
       <div class="col">
-        <div class="bg-black card h-100">
+        <div class="bg-black card h-100" className="portfolio" style={styles.portfolioStyles}>
           <img src="https://mdbcdn.b-cdn.net/img/new/standard/city/042.webp" class="card-img-top" alt="Palm Springs Road" />
           <div class="card-body">
-            <h5 class="card-title text-white text-center">Card title</h5>
+            <h5 class="card-title text-white text-center p-4">Card title</h5>
             <p class="card-text text-white">Project 2</p>
-            <a type="button" class="btn btn-light btn-rounded" href="https://github.com/" style={styles.portfolioStyles}>Light</a>
-            <a type="button" class="btn btn-light btn-rounded" href="https://github.com/" style={styles.portfolioStyles}>Light</a>
+            <a type="button" class="btn btn-light btn-rounded" href="https://github.com/" >Light</a>
+            <a type="button" class="btn btn-light btn-rounded" href="https://github.com/" >Light</a>
           </div>
         </div>
       </div>
       <div class="col">
-        <div class="bg-black card h-100">
+        <div class="bg-black card h-100" className="portfolio" style={styles.portfolioStyles}>
           <img src="https://mdbcdn.b-cdn.net/img/new/standard/city/043.webp" class="card-img-top" alt="Los Angeles Skyscrapers" />
           <div class="card-body">
-            <h5 class="card-title text-white text-center">Card title</h5>
+            <h5 class="card-title text-white text-center p-4">Card title</h5>
             <p class="card-text text-white">Project 3</p>
             <a type="button" class="btn btn-light btn-rounded" href="https://github.com/" style={styles.portfolioStyles}>Light</a>
             <a type="button" class="btn btn-light btn-rounded" href="https://github.com/" style={styles.portfolioStyles}>Light</a>
@@ -52,10 +51,10 @@ export default function Portfolio() {
         </div>
       </div>
       <div class="col">
-        <div class="bg-black card h-100">
+        <div class="bg-black card h-100" className="portfolio" style={styles.portfolioStyles}>
           <img src="https://mdbcdn.b-cdn.net/img/new/standard/city/044.webp" class="card-img-top" alt="Skyscrapers" />
           <div class="card-body">
-            <h5 class="card-title text-center text-white">Card title</h5>
+            <h5 class="card-title text-center text-white p-4">Card title</h5>
             <p class="card-text text-white">
               Project 4
             </p>
@@ -65,10 +64,10 @@ export default function Portfolio() {
         </div>
       </div>
       <div class="col">
-        <div class="bg-black card h-100">
+        <div class="bg-black card h-100" className="portfolio" style={styles.portfolioStyles}>
           <img src="https://mdbcdn.b-cdn.net/img/new/standard/city/043.webp" class="card-img-top" alt="Los Angeles Skyscrapers" />
           <div class="card-body">
-            <h5 class="card-title text-center text-white">Card title</h5>
+            <h5 class="card-title text-center text-white p-4">Card title</h5>
             <p class="card-text text-white">Project 3</p>
             <a type="button" class="btn btn-light btn-rounded" href="https://github.com/" style={styles.portfolioStyles}>Light</a>
             <a type="button" class="btn btn-light btn-rounded" href="https://github.com/" style={styles.portfolioStyles}>Light</a>
@@ -76,10 +75,10 @@ export default function Portfolio() {
         </div>
       </div>
       <div class="col">
-        <div class="bg-black card h-100">
+        <div class="bg-black card h-100" className="portfolio" style={styles.portfolioStyles}>
           <img src="https://mdbcdn.b-cdn.net/img/new/standard/city/043.webp" class="card-img-top" alt="Los Angeles Skyscrapers" />
           <div class="card-body">
-            <h5 class="card-title text-center text-white">Card title</h5>
+            <h5 class="card-title text-center text-white p-4">Card title</h5>
             <p class="card-text text-white">Project 3</p>
             <a type="button" class="btn btn-light btn-rounded" href="https://github.com/" style={styles.portfolioStyles}>Light</a>
             <a type="button" class="btn btn-light btn-rounded" href="https://github.com/" style={styles.portfolioStyles}>Light</a>
